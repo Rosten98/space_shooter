d78b4c440525fcb5419be9c2eff19efc43cd985e
Gerardo <gerodel98@gmail.com>
2020-01-25 15:50:52 -0600

    Commented explosion sound

--- sound.py
+++ sound.py
@@ -3,10 +3,13 @@ import constants as consts
 
 class Sound():
     def __init__(self):
-        pass
+        self.shot = pygame.mixer.Sound(consts.LASER1)
+
+
     def init_mixer(self):
         pygame.mixer.init()
         self.main_theme()
+
 
     def main_theme(self):
         self.main_theme = pygame.mixer.Sound(consts.MAIN_THEME)
@@ -14,13 +17,14 @@ class Sound():
 
 
     def shoot(self):
-        self.shot = pygame.mixer.Sound(consts.LASER1)
+
         self.shot.play(0)
 
 
     def explosion(self):
-        self.shot = pygame.mixer.Sound(consts.EXPLOSION)
-        self.shot.play(0)
+        # self.shot = pygame.mixer.Sound(consts.EXPLOSION)
+        # self.shot.play(0)
+        pass
 
     def quit_mixer(self):
         pygame.mixer.stop()
