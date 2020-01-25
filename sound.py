5fe97828ad26d5da6ef2c9b9019dffc5be355b63
Gerardo <gerodel98@gmail.com>
2020-01-25 12:38:35 -0600

    Added sound class and laser sound file

---
+++ sound.py
@@ -0,0 +1,18 @@
+import pygame
+import constants as consts
+
+class Sound():
+    def __init__(self):
+        pygame.mixer.init()
+
+
+    def main_theme(self):
+        pass
+
+
+    def shoot(self):
+        play.mixer.music.load(consts.LASER1_SOUND)
+
+
+    def explosion(self):
+        pass
